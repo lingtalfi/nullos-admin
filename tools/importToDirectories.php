a67367c4b2301317610026b74ff5be81671313d0
lingtalfi <lingtalfi@gmail.com>
2016-11-27 14:58:22 +0100

    automatic commit from task manager

---
+++ tools/importToDirectories.php
@@ -0,0 +1,33 @@
+<?php
+
+
+use Bat\FileSystemTool;
+
+require "bigbang.php";
+
+
+$importFileDir = "/pathto/php/projects/nullos-admin/app-nullos/class-planets";
+$planetsDir = "/pathto/php/projects/universe-snapshots/planets";
+
+
+$files = file($importFileDir . "/_import.txt", \FILE_IGNORE_NEW_LINES);
+
+foreach ($files as $dir) {
+
+
+    $oldFile = $importFileDir . "/$dir";
+
+    if (file_exists($oldFile)) {
+        if (is_link($oldFile)) {
+            unlink($oldFile);
+        } else {
+            FileSystemTool::remove($oldFile);
+        }
+    }
+
+
+    $planetDir = $planetsDir . "/$dir";
+    FileSystemTool::copyDir($planetDir, $oldFile);
+}
+
+
